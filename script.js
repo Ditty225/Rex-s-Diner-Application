86b2a0f88c2aff844c21ed9bb17fd94f9df04251
Ditty225 <126191602+Ditty225@users.noreply.github.com>
2024-04-17 12:40:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,27 +10,32 @@ function submitForm() {
     }
 
     // Constructing the message
-    let messageContent = `**New Job Application**\n`;
-    for (let key in object) {
-        if (object.hasOwnProperty(key) && object[key] !== '') {
-            messageContent += `- ${key.charAt(0).toUpperCase() + key.slice(1)}: ${object[key]}\n`;
+    var message = {
+        content: 'New job application submitted!',
+        embeds: [{
+            title: 'Job Application Details',
+            fields: Object.keys(object).filter(key => object[key] !== '').map(key => {
+                return {
+                    name: key.charAt(0).toUpperCase() + key.slice(1),
+                    value: object[key],
+                    inline: true
+                };
+            })
+        }]
+    };
+
+    // Discord webhook integration
+    var discordWebhookURL = 'https://discord.com/api/webhooks/1229932797750284400/z2X-9PHxqaJ7H-mHEP55Lf234eduw34XOq7jFbDsudYPyj5csxkCRAheCOWtAstJ7BRl';
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', discordWebhookURL, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 204) {
+            alert('Application submitted successfully!');
+        } else if (xhr.readyState === 4) {
+            alert('Failed to submit application: ' + xhr.status);
         }
-    }
-    
-    // Send the data to Discord
-    fetch('https://discord.com/api/webhooks/1230204645989814364/CLdFj2GMNxTU_f3Cs25Z636zhVQmEaRYKAMAxLr7Z6VlxH2nx8OyxGROOlHcO9_Ozwsy', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            content: messageContent
-        })
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok: ' + response.status);
-        }
-        return response.json();
-    })
-    .then(data => alert('Application submitted successfully!'))
-    .catch((error) => alert('Failed to submit application: ' + error));
+    };
+    xhr.send(JSON.stringify(message));
 }
+
